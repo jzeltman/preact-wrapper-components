3342ea5174bc53b5e76cb6e3cf738dbdfdb1e2ad
Josh Zeltman <joshua.zeltman@gmail.com>
2019-11-15 18:35:21 -0300

    version bump. compiled files. added babel + webpack to compile files

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve('lib'),
+        filename: 'index.js',
+        libraryTarget: 'commonjs2'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                exclude: /(node_modules)/,
+                use: 'babel-loader'
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            react: 'preact-compat',
+            'react-dom': 'preact-compat'
+        }
+    }
+}
