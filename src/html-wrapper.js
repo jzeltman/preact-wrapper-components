445c69e6d98bf540c1aa9bc0079bd0b5b0f664cc
Josh Zeltman <joshua.zeltman@gmail.com>
2019-11-20 11:46:35 -0300

    attempting to make this work

--- src/html-wrapper.js
+++ src/html-wrapper.js
@@ -1,17 +1,18 @@
-import { h, Component } from 'preact';
+import { h, Component } from "preact";
 
 export default class HTMLWrapper extends Component {
     constructor() {
         super();
         this.getMarkup.bind(this);
-        this.state = { html: null };
     }
-
+  
     getMarkup(prop) {
         return { __html: prop };
     }
-
+  
     render() {
-        return <div dangerouslySetInnerHTML={this.getMarkup(this.props.html)}></div>;
+        return (
+            <div dangerouslySetInnerHTML={this.getMarkup(this.props.html)}></div>
+        )
     }
 };
--- webpack.config.js
+++ webpack.config.js
@@ -17,9 +17,9 @@ module.exports = {
             }
         ]
     },
-    resolve: {
-        alias: {
-            react: 'preact-compat',
+    'resolve': {
+        'alias': {
+            'react': 'preact-compat',
             'react-dom': 'preact-compat'
         }
     }
